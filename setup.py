173aae2763e79daa503fc91ab727cf09f30dba5b
Iñaki Silanes <isilanes@gmail.com>
2018-05-22 12:26:26 +0200

    fixes for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup
 
+
+def long_description():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
 setup(
     name="logworks",
     packages=["logworks"],
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    description="Logworks module",
+    description="logworks is a convenience wrapper for the logging Python module.",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
     url="https://github.com/isilanes/logworks",
     author="Iñaki Silanes Cristóbal",
     author_email="isilanes@gmail.com",
     license="GPLv3",
-    keywords = ["logging"],
-    classifiers = [],
+    keywords=["logging"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPLv3 License",
+        "Operating System :: OS Independent",
+    ),
     zip_safe=False
 )
