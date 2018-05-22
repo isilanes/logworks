3a363911fb967cedff493b28141ae89aa1fa947c
Iñaki Silanes <isilanes@gmail.com>
2018-05-22 11:27:47 +0200

    minor changes for PyPI

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name="logworks",
+    packages=["logworks"],
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Logworks module",
@@ -9,6 +10,7 @@ setup(
     author="Iñaki Silanes Cristóbal",
     author_email="isilanes@gmail.com",
     license="GPLv3",
-    packages=["logworks"],
+    keywords = ["logging"],
+    classifiers = [],
     zip_safe=False
 )
